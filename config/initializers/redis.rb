f4efd56faf416300bda9eaed7553d91f289714c0
umakantrajpoot <rajput.umakant4@gmail.com>
2014-12-31 00:13:03 +0530

    add redis url

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,2 +1,2 @@
-uri = URI.parse(ENV["REDISTOGO_URL"] || "redis://localhost:6379/" )
+uri = URI.parse(ENV["REDISTOGO_URL"] || "redis://redistogo:f78b6bdc2d054442c78ae6d8895daf4d@mummichog.redistogo.com:9547/")
 REDIS = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
