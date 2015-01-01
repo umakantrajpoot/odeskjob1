2f92c85cc08f4ce339ea2c3e2e4e7d6e82f02d8f
umakantrajpoot <rajput.umakant4@gmail.com>
2015-01-01 08:21:27 +0530

    explicitely  delete tmp file

--- app/workers/listing_worker.rb
+++ app/workers/listing_worker.rb
@@ -3,6 +3,9 @@ class ListingWorker
   def perform(photos, listing_id)
     photos.each do | photo |
       Photo.create!(:image => File.open(photo), :listing_id => listing_id)
+      if !File.open(photo).nil?
+        File.delete(photo)
+      end
     end
   end
 end
