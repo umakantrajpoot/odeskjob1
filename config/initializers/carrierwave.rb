836fe7dace082da92969241b7b32390e8138a715
umakantrajpoot <rajput.umakant4@gmail.com>
2015-01-01 11:28:16 +0530

    add carrierwave tmp folder

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,16 +1,15 @@
 CarrierWave.configure do |config|
-
-    config.fog_credentials = {
-      :provider                 => 'AWS',
-      :aws_access_key_id        => ENV['AWS_ACCESS_KEY_ID'],
-      :aws_secret_access_key    => ENV['AWS_SECRET_ACCESS_KEY'],
-      :region                   => ENV['FOG_REGION']
-    }
-    config.fog_directory   = ENV['FOG_DIRECTORY']
-    config.fog_public      = true
-    config.fog_attributes  = {'Cache-Control' => 'max-age=315576000'}
-    # if Rails.env.production?
-    #   config.asset_host      = ""
-    # end
-
+  config.cache_dir = "#{Rails.root}/tmp/uploads"
+  config.fog_credentials = {
+    :provider                 => 'AWS',
+    :aws_access_key_id        => ENV['AWS_ACCESS_KEY_ID'],
+    :aws_secret_access_key    => ENV['AWS_SECRET_ACCESS_KEY'],
+    :region                   => ENV['FOG_REGION']
+  }
+  config.fog_directory   = ENV['FOG_DIRECTORY']
+  config.fog_public      = true
+  config.fog_attributes  = {'Cache-Control' => 'max-age=315576000'}
+  # if Rails.env.production?
+  #   config.asset_host      = ""
+  # end
 end
